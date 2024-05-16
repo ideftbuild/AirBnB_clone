a088bc8ea22ebe35dee61ef8c8f1489d1171477e
akanSWE <a.akanSWE@gamil.com>
2024-05-16 13:11:48 +0100

    refactor(Console): improve documentations and commenting

--- console.py
+++ console.py
@@ -38,15 +38,15 @@ class HBNBCommand(cmd.Cmd):
     An interactive command line interpreter
 
     Methods:
-        do_quit: command to exit the program
-        do_EOF: command to exit the program
-        do_all: prints all instance or instances of a particular class
-        do_create: creates an instance of a particular class
-        do_show: prints an instance
-        do_destroy: deletes an instance
-        do_update: updates an instance
-        parse_line: parses the line passed and checks for any invalid inputs
-        emptyline: does nothing when an empty line + Enter is entered
+    - emptyline(): Does nothing when an empty line is entered.
+    - parse_line(line): Parses the input line and validates inputs.
+    - do_quit(): Exits the program.
+    - do_EOF(): Exits the program, when the EOF signal is received
+    - do_all(line): Prints all instances or instances of a specific class.
+    - do_create(line): Creates a new instance with given attributes.
+    - do_show(line): Prints details of a specific instance.
+    - do_destroy(line): Deletes a specific instance.
+    - do_update(line): Updates or add an attribute of a specific instance.
     """
     prompt = "(hbnb) "
 
@@ -54,10 +54,18 @@ class HBNBCommand(cmd.Cmd):
         """
         Name
         ----
-            emptyline - handle an empty line
+            emptyline
+
         Synopsis
         --------
-            Called when an empty line or a line with spaces is entered
+            Handle an empty line or a line containing only spaces
+
+        Description
+        -----------
+            This method is called when an empty line or a line with spaces is
+            encountered. It performs any necessary actions or processing
+            specific to empty lines.
+
         """
         return False
 
@@ -66,23 +74,23 @@ class HBNBCommand(cmd.Cmd):
         """
         Tokenize the input line and perform optional checks.
 
-        All checks are disable by defaults and will be enabled by caller.
+        All checks are disabled by default and can be enabled by the caller.
 
         Args:
-            line: The input line to be tokenized.
-            m: Flag to enable check for missing class.
-            c: Flag to enable check for class.
-            i: Flag to enable check for ID.
-            n: Flag to enable check for instance.
-            a: Flag to enable check for attribute.
-            v: Flag to enable check for value.
+            line (str): The input line to be tokenized.
+            m (bool): Flag to enable check for missing class.
+            c (bool): Flag to enable check for class.
+            i (bool): Flag to enable check for ID.
+            n (bool): Flag to enable check for instance.
+            a (bool): Flag to enable check for attribute.
+            v (bool): Flag to enable check for value.
 
         Returns:
-            list: Tokens extracted from the input line otherwise None
-            if an error occurs.
+            list: Tokens extracted from the input line.
+            Returns None if an error occurs.
         """
         from shlex import split
-
+        # check for errors based on flags pass
         tokens = split(line)
         if m and line == '':
             print('** class name missing **')
@@ -97,7 +105,7 @@ class HBNBCommand(cmd.Cmd):
         elif v and len(tokens) < 4:
             print('** value missing **')
         else:
-            return tokens
+            return tokens  # success, all checks pass
 
     def do_quit(self, line):
         """
@@ -108,6 +116,11 @@ class HBNBCommand(cmd.Cmd):
         Synopsis
         --------
             quit
+
+        Description
+        -----------
+            This method is called to terminate the program ensuring
+            proper cleanup and resource deallocation.
         """
         return True
 
@@ -118,12 +131,23 @@ class HBNBCommand(cmd.Cmd):
         """
         Name
         ----
-            EOF - exit the program
+            EOF - end-of-file signal
 
         Synopsis
         --------
-            Ctrl+D (Linux)
-            Ctrl+Z followed by Enter (Windows)
+            Exit the program using the appropriate keyboard shortcut
+
+            Linux: Ctrl+D
+            Windows: Ctrl+Z followed by Enter
+
+        Description
+        ----------
+            This method is called when the end-of-file (EOF) signal is
+            received, typically triggered by the user using the specified
+            keyboard shortcuts. It allows for graceful termination of the
+            program, similar to the "quit" method, ensuring proper cleanup
+            and resource deallocation.
+
         """
         print()
         return True
@@ -143,10 +167,16 @@ class HBNBCommand(cmd.Cmd):
 
         Description
         ----------
-            Creates a new instance, saves it (to the JSON file)
-            and prints the id.
+            This method creates a new instance of the specified class and
+            saves it to a JSON file. It then prints the ID of the newly
+            created instance.
 
-            For errors refer to the ERRORS section for errors that may occur.
+        Examples
+        --------
+            `create User`
+
+            This will create a new instance of the "User" class and save it
+            to the JSON file.
 
         Errors
         ------
@@ -174,10 +204,16 @@ class HBNBCommand(cmd.Cmd):
 
         Description
         -----------
-            Prints the string representation of an instance based on the
-            class name and id.
+            This method prints the string representation of an instance based
+            on the provided class name and ID. It retrieves the instance from
+            the JSON file and displays its details.
 
-            For errors refer to the ERRORS section for errors that may occur.
+        Examples
+        --------
+            `show Place 1`
+
+            This will print the details of instance with ID "1" of the "Place"
+            class.
 
         Errors
         ------
@@ -205,10 +241,16 @@ class HBNBCommand(cmd.Cmd):
 
         Description
         ----------
-            Deletes an instance based on the class name and id
-            (save the change into the JSON file).
+            This method deletes an instance of the specified class based on
+            the provided ID. The deletion is permanent and the change is
+            saved to the JSON file.
 
-            For errors refer to the ERRORS section for errors that may occur.
+        Examples
+        --------
+            `destroy City 1`
+
+            This will delete the instance with ID "1" of the "City" class and
+            save the change to the JSON file.
 
         Errors
         ------
@@ -216,10 +258,6 @@ class HBNBCommand(cmd.Cmd):
             `** class doesn't exit`     [class name] isn't supported.
             `** instance id missing **` [id] is not given.
             `** no instance found **`   The object with [id] isn't available .
-
-        Notes
-        -----
-            Instance that are deleted can't be recovered.
         """
         tokens = self.parse_line(line, m=True, c=True, i=True, n=True)
         if tokens:
@@ -242,20 +280,19 @@ class HBNBCommand(cmd.Cmd):
 
         Description
         -----------
-            Prints all string representation of all instances based or not
-            on the class name.
-
-            For errors refer to the ERRORS section for errors that may occur.
+            This method prints the string representation of all instances,
+            optionally filtered by the provided class name. It retrieves
+            the instances from the JSON file and displays their details.
 
         Errors
         ------
-            `** class doesn't exit`     [class name] isn't supported.
+            `** class doesn't exit` [class name] isn't supported.
         """
         objs = models.storage.all().values()
         objs_list = None
-        if line == '':
+        if line == '':  # all
             objs_list = [str(obj) for obj in objs]
-        else:
+        else:  # all [class name]
             tokens = self.parse_line(line, m=True, c=True)
             if tokens:
                 objs_list = [str(obj) for obj in objs
@@ -279,10 +316,17 @@ class HBNBCommand(cmd.Cmd):
 
         Description
         -----------
-            Updates an instance based on the class name and id by adding or
-            updating attribute (save the change into the JSON file).
+            This method updates an instance of the specified class based on
+            the provided ID by adding or modifying an attribute.
+            The change is saved to the JSON file.
 
-            For errors refer to the ERRORS section for errors that may occur.
+        Examples
+        --------
+            ``update Place 1 name Lagos`
+
+            This will update the "name" attribute of the instance with ID "1"
+            of the "Place" class to "Lagos" and save the change to the JSON
+            file.
 
         Errors
         ------
@@ -304,6 +348,7 @@ class HBNBCommand(cmd.Cmd):
                     tokens[3] = float(tokens[3])
                 except ValueError:
                     pass
+            # update or add the attribute and it value
             obj = models.storage.all()[f'{tokens[0]}.{tokens[1]}']
             obj.__dict__.update({tokens[2]: tokens[3]})
             obj.save()
