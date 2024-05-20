19978c4c93fd053d9aa47b22a1c420181c922005
akanSWE <a.akanSWE@gamil.com>
2024-05-20 11:57:12 +0100

    update test for base model

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,12 +1,31 @@
 #!/usr/bin/env python3
 import unittest
 from models.base_model import BaseModel
-from models import storage
+from models.engine.file_storage import FileStorage
 from datetime import datetime
+
+
+FileStorage._FileStorage__file_path = 'test_file.json'
 
 
 class TestBaseModel(unittest.TestCase):
     """Unittest testing for the BaseModel class"""
+
+    def setUp(self):
+        """Create new instance of FileStorage for each test and reset
+        __objects attribute to an empty dictionary
+        """
+        self.storage = FileStorage()
+        self.storage._FileStorage__objects = {}
+
+    def tearDown(self):
+        """Delete the JSON file that is created by a test"""
+        from os import path, remove
+
+        json_file = self.storage._FileStorage__file_path
+        if path.isfile(json_file):
+            remove(json_file)
+
     def test_uuid(self):
         """Test if uuid is a string and also unique"""
         bm1 = BaseModel()
@@ -38,7 +57,6 @@ class TestBaseModel(unittest.TestCase):
             - created_at and updated_at are converted to datetime
         """
         bm1 = BaseModel()
-        # model_dict = bm1.to_dict()
         model_dict = {"id": "062d5ee4-2fc8-4a1a-8205-341ce160ea4d",
                       "updated_at": "2024-05-13T11:56:30.092401",
                       "created_at": "2024-05-13T11:56:30.092340",
@@ -48,7 +66,6 @@ class TestBaseModel(unittest.TestCase):
         self.assertTrue(hasattr(bm1, "id"))  # check if id is present
         # should not contain the __class__ key or attribute
         self.assertFalse(bm1.__dict__.__contains__("__class__"))
-        # self.assertEqual(bm1.id, bm2.id)
         self.assertIsInstance(bm1.created_at, datetime)
         self.assertIsInstance(bm1.updated_at, datetime)
 
@@ -67,12 +84,12 @@ class TestBaseModel(unittest.TestCase):
         self.assertIsInstance(bm2_dict, dict)
         self.assertIsInstance(bm2_dict["created_at"], str)
 
-    def test_save(self):
-        import os
-
+    def test_update_at_is_updated(self):
+        """Verify that 'updated_at' is updated to the current datetime
+        when save is called"""
         bm = BaseModel()
+        old_updated_at = bm.updated_at
         bm.save()
 
-        file = storage._FileStorage__file_path
-        self.assertTrue(os.path.exists(file))
-        self.assertTrue(os.path.getsize(file) > 0)
+        self.assertNotEqual(bm.updated_at, old_updated_at)
+        self.assertGreater(bm.updated_at, old_updated_at)
